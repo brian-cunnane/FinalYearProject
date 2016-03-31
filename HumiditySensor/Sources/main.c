ec39928424ac625c852b02c27d7dc8ce70bb8e9e
BRIAN CUNNANE - Student <G00174469@gmit.ie>
2016-03-31 16:40:29 +0100

    Converted reading from register values to relative humidity. Readings were noisy so averaged over 10 readings

--- HumiditySensor/Sources/main.c
+++ HumiditySensor/Sources/main.c
@@ -11,26 +11,36 @@
 #include "fsl_debug_console.h"
 #include <stdlib.h>
 
-
 #define HUMIDITYCHANNEL 14
 
-int main(void)
-{
+int count = 0;
+
+int main(void) {
 	hardware_init();
-	adc0_config(SW_TRIGGER,BIT16,ADC_INTERRUPT_DISABLED);
+	adc0_config(SW_TRIGGER, BIT16, ADC_INTERRUPT_DISABLED);
 	PIT_Configure_interrupt_mode(1);
 	PRINTF("~~~Starting ADC read~~~\r\n");
-	while(1){}
+	while (1) {
+	}
 
 	return 0;
 }
 
-void PIT_IRQHandler()
-{
+void PIT_IRQHandler() {
 	PIT_TFLG0 = 0x01ul;
 	ADC0_SC1A = 0x04Fu;
 	ADC0_SC1A &= 0xFFFFFFE0;
 	ADC0_SC1A |= HUMIDITYCHANNEL;
-	int sample = read_adc0(HUMIDITYCHANNEL);
-	PRINTF("Sample is %d\r\n",sample);
+	float average;
+	unsigned int sample = read_adc0(HUMIDITYCHANNEL);
+	float relativeHumidity = (float) sample - 32981.97818;
+	relativeHumidity = relativeHumidity * 0.00162;
+	average += relativeHumidity;
+	count ++;
+	if (count == 10)//take 10 readings and find average to cut down noise.
+	{
+		PRINTF("Sample is %d. RH %d%% \r\n", sample, (unsigned int) average/10);
+		average = 0;
+		count = 0;
+	}
 }
