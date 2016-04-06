5766ef2701b3a02b1190b1be8b513ebe32b8ef9d
BRIAN CUNNANE - Student <G00174469@gmit.ie>
2016-04-06 18:24:56 +0100

    Cleaned up code. Moved sensor read into a function. Added function to create calibration factor. Read is now average of 50 reads to reduce noise

--- Weight/Sources/main.c
+++ Weight/Sources/main.c
@@ -16,62 +16,79 @@
  * toggle clock once to reset
  * wait for data to go high before rereading
  */
+unsigned long int readValue();
+unsigned long int calibrate();
+
+unsigned long int calibration_factor = 0;
 
 int main(void)
 {
-	long int sample = 0;
+	int i;
+	unsigned long int averageValue = 0;
 	hardware_init();
 	FRDM_KL26Z_CLK_Configure();//configure PTC8 as output for clocking hx711
 	FRDM_KL26Z_DATA_Configure(0,0);//configure PTC9 as input for HX711
 	PRINTF("Weighing scales test code\r\n");
-	char data[24] = {0};
+	PRINTF("Calibrating\r\n");
+	calibration_factor = calibrate();
+	PRINTF("Calibration factor: %ld\r\n",calibration_factor);
+
     while(1)
     {
-    	int x;
-    	int i;
+    	for(i = 0; i < 50; i ++)
+    		averageValue += readValue();
+    	averageValue = averageValue / 50;
+    	PRINTF("\r\nSample is %ld \r\n",averageValue-calibration_factor);
 
-    	GPIOC_PCOR |= CLK_MASK; //PTC8 = 0
-    	while(DATA_read());
-    	for(x = 0; x < 24; x++)
-    	{
-    		//GPIOC_PCOR |= CLK_MASK; //PTC8 = 0
-    		GPIOC_PSOR |= CLK_MASK; //PTC8 = 1
-    		for(i = 0; i < 200; i ++); //delay
-    		//GPIOC_PCOR |= CLK_MASK;
-    		//for(i = 0; i < 200; i ++);
-    		//while(DATA_read());
-    		if(DATA_read())
-    		{
-    			data[x] = 0x01u;//read data into char array to see binary value
-    			sample |= 0x01u;//read data into long int.
-    		}
-    		sample <<= 1;
-    		GPIOC_PCOR |= CLK_MASK;
-    		for(i = 0; i < 200; i ++);
-
-    	}
-    	sample >>= 1;
-    	GPIOC_PSOR |= CLK_MASK;
-    	for(i = 0; i < 200; i ++);
-    	GPIOC_PCOR |= CLK_MASK;
-    	//sample ^= 0x800000; convert from 2s compliment
-    	/*PRINTF("SAMPLE is %ld\r\n",sample);
-    	int sign = sample >> 23;
-    	PRINTF("sign: %d\r\n",sign);
-    	if(sign == 1){
-    		PRINTF("IT'S TWO'S COMPLIMENT\r\n");
-    		unsigned long int value = (sample + sign) ^ sign;
-    		PRINTF("Value is %lu\r\n",value);
-    	}
-    	*/
-    	for(x = 0; x < 24; x ++)
-    	{
-    		PRINTF("%d", data[x]);
-    		data[x] = 0x00u;
-    	}
-    	PRINTF("\r\nSample is %ld \r\n",sample);
-    	sample = 0;
     }
 
     return 0;
+}
+
+unsigned long int calibrate()
+{
+	unsigned long int average =0;
+	int i;
+	for(i = 0; i < 50; i ++)
+	{
+		average += readValue();
+	}
+	average = average / 50;
+	return average;
+}
+
+unsigned long int readValue()
+{
+	int x;
+	int i;
+	unsigned long int sample = 0;
+	char data[24] = {0};
+	GPIOC_PCOR |= CLK_MASK; //PTC8 = 0
+	while(DATA_read());
+	for(x = 0; x < 24; x++)
+	{
+		GPIOC_PSOR |= CLK_MASK; //PTC8 = 1
+		for(i = 0; i < 200; i ++); //delay
+		if(DATA_read())
+		{
+			data[x] = 0x01u;//read data into char array to see binary value
+			sample |= 0x01u;//read data into long int.
+		}
+		sample <<= 1;
+		GPIOC_PCOR |= CLK_MASK;
+		for(i = 0; i < 200; i ++);
+
+	}
+	sample >>= 1;
+	GPIOC_PSOR |= CLK_MASK;
+	for(i = 0; i < 200; i ++);
+	GPIOC_PCOR |= CLK_MASK;
+
+	/*for(x = 0; x < 24; x ++)
+	{
+		PRINTF("%d", data[x]);
+		data[x] = 0x00u;
+	}*/
+	sample ^= 0x800000;
+	return sample;
 }
