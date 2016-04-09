488ee766ed1f3dfd532f85fa0bcce73699a3b0d2
Brian Cunnane <g00174469@gmit.ie>
2016-04-09 21:49:57 +0100

    Fully migrated to header file system.

--- Weight/Sources/WeightSensor.h
+++ Weight/Sources/WeightSensor.h
@@ -2,7 +2,7 @@
  * WeightSensor.h
  *
  *  Created on: 9 Apr 2016
- *      Author: brian
+ *      Author: Brian Cunnane
  */
 
 #ifndef SOURCES_WEIGHTSENSOR_H_
@@ -12,6 +12,13 @@ unsigned long int calibrate();
 unsigned long int readValue();
 unsigned long int readAverageValue(unsigned long int av, unsigned long int zero_offset);
 
+/**************************************************************************************
+ *
+ * Sample and sum data 256 times.
+ * Divide by number of samples.
+ * Return average value which corresponds to the weight of the hardware i.e. the zero offset
+ *
+ **************************************************************************************/
 unsigned long int calibrate() {
 	unsigned long int average = 0;
 	int i;
@@ -21,20 +28,33 @@ unsigned long int calibrate() {
 	return average;
 }
 
+
+/*************************************************************************************
+ *
+ * Set clock low.
+ * Wait for data line to go low.
+ * Set clock high.
+ * Delay to allow settling
+ * If data line is high, or 1 bit into sample value
+ * Shift sample 1 bit
+ * Set clock low
+ * Delay
+ * Shift sample one bit right to remove extra 0
+ * Toggle clock to reset conversion
+ * XOR MSB of sample with 1 to remove sign.
+ *
+ *************************************************************************************/
 unsigned long int readValue() {
 	int x;
 	int i;
 	unsigned long int sample = 0;
-	char data[24] = { 0 };
 	GPIOC_PCOR |= CLK_MASK; //PTC8 = 0
 	while (DATA_read());
 	for (x = 0; x < 24; x++) {
 		GPIOC_PSOR |= CLK_MASK; //PTC8 = 1
 		for (i = 0; i < 200; i++); //delay
-		if (DATA_read()) {
-			data[x] = 0x01u; //read data into char array to see binary value
+		if (DATA_read())
 			sample |= 0x01u; //read data into long int.
-		}
 		sample <<= 1;
 		GPIOC_PCOR |= CLK_MASK;
 		for (i = 0; i < 200; i++);
@@ -43,10 +63,17 @@ unsigned long int readValue() {
 	GPIOC_PSOR |= CLK_MASK;
 	for (i = 0; i < 200; i++);
 	GPIOC_PCOR |= CLK_MASK;
-	sample ^= 0x800000;
+	sample ^= 0x800000;//data is in two's compliment so remove the sign.
 	return sample;
 }
 
+/***************************************************************************************
+ *
+ * Sample and sum the data 256 times.
+ * Minus the zero offset calculated during calibration.
+ * Devide sum by 256 and return value
+ *
+ ***************************************************************************************/
 unsigned long int readAverageValue(unsigned long int av, unsigned long int zero_offset){
 	int i;
 	unsigned long int value;
--- Weight/Sources/main.c
+++ Weight/Sources/main.c
@@ -20,16 +20,11 @@
  * wait for data to go high before rereading
  */
 
-//unsigned long int calibrate();
-//unsigned long int readValue();
-//unsigned long int readAverageValue(unsigned long int av);
-
-unsigned long int zero_offset = 0;
-
 int main(void) {
 	int i;
 	int value;
 	int grams = 0;
+	unsigned long int zero_offset = 0;
 	unsigned long int averageValue = 0;
 
 	hardware_init();
@@ -54,51 +49,3 @@ int main(void) {
 	}
 	return 0;
 }
-/*
-unsigned long int calibrate() {
-	unsigned long int average = 0;
-	int i;
-	for (i = 0; i < 256; i++)
-		average += readValue();
-	average = average / 256;
-	return average;
-}
-
-unsigned long int readValue() {
-	int x;
-	int i;
-	unsigned long int sample = 0;
-	char data[24] = { 0 };
-	GPIOC_PCOR |= CLK_MASK; //PTC8 = 0
-	while (DATA_read());
-	for (x = 0; x < 24; x++) {
-		GPIOC_PSOR |= CLK_MASK; //PTC8 = 1
-		for (i = 0; i < 200; i++); //delay
-		if (DATA_read()) {
-			data[x] = 0x01u; //read data into char array to see binary value
-			sample |= 0x01u; //read data into long int.
-		}
-		sample <<= 1;
-		GPIOC_PCOR |= CLK_MASK;
-		for (i = 0; i < 200; i++);
-	}
-	sample >>= 1;
-	GPIOC_PSOR |= CLK_MASK;
-	for (i = 0; i < 200; i++);
-	GPIOC_PCOR |= CLK_MASK;
-	sample ^= 0x800000;
-	return sample;
-}
-
-unsigned long int readAverageValue(unsigned long int av){
-	int i;
-	unsigned long int value;
-	for (i = 0; i < 256; i++){
-				value = readValue();
-				av = av + value - zero_offset;
-				PRINTF("\r\n\t\t%ld",value);
-			}
-			av = av / 256;
-			return av;
-}
-*/
