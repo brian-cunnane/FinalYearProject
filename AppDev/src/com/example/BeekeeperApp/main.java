95c289d853a4cb94fe6d0060c19c7dab2effd34b
Brian Cunnane <g00174469@gmit.ie>
2016-04-19 12:47:11 +0100

    Changed from thread to asynch task to fetch the data from db. json array is output to screen. needs to be parsed and graphed

--- AppDev/src/com/example/BeekeeperApp/main.java
+++ AppDev/src/com/example/BeekeeperApp/main.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import org.apache.http.client.ClientProtocolException;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -42,32 +45,51 @@ public class main extends Activity {
                 NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
                 if(networkInfo != null && networkInfo.isConnected()){
                     Toast.makeText(getApplicationContext(),"Connected",Toast.LENGTH_LONG).show();
-                    String URL = "http://192.168.0.22:8080/beekeeper/test";
+                    JSONObject test = new JSONObject();
+                    try {
+                        test.put("test","1");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
 
-                    JSONObject jsonInput = new JSONObject(inputMap);
-                    new Thread(new Runnable(){
-                        @Override
-                        public void run(){
-                            try{
-                                /*String query = "Select * from HIVE1";*/
-                                String jsonString = HttpUtils.urlContentPost(URL,"all",jsonInput.toString());
-                                jsonResult = new JSONObject(jsonString);
-                            }catch(ClientProtocolException CPE){CPE.printStackTrace();}
-                            catch(IOException IOE){Toast.makeText(getApplicationContext(),"IO Exception",Toast.LENGTH_LONG).show();}
-                            catch(JSONException JSE){Toast.makeText(getApplicationContext(),"JSON Exception",Toast.LENGTH_LONG).show();}
-                        }
-                    }).start();
-                    try{
-                        textView1.setText(URL + "\n" + jsonInput.toString() + "\n");
-                        //TODO jsonResult is returning empty
-                        textView1.setText(textView1.getText()+jsonResult.getString("all"));
-                    }catch(Exception EE){EE.printStackTrace();}
-
+                    //String URL = "http://10.12.11.185:8080/test";
+                    new HttpAsyncTask().execute("http://10.12.11.185:8080/android?test=test",test.toString());
                 }
                 else
                     Toast.makeText(getApplicationContext(),"Not Connected",Toast.LENGTH_LONG).show();
                 //String url = "http://10.12.24.8";
             }
         });
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+           // String URL = "http://10.12.11.185:8080/test";
+            String jsonString = "";
+            try {
+                jsonString = HttpUtils.urlContentPost(params[0],"test",params[1]);
+                Log.d("url",jsonString.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return jsonString;
+        }
+
+        protected void onPostExecute(String result){
+            JSONArray jsonArray = new JSONArray();
+            JSONObject jsonResult = new JSONObject();
+            try{
+                jsonArray = new JSONArray(result);
+                textView1.setText(jsonArray.toString());
+                //jsonResult = new JSONObject(result);
+            }catch(JSONException JE){
+                JE.printStackTrace();
+            }
+            try{
+                String temp = jsonResult.getString("temperature");
+                textView1.setText(textView1.getText() + "\n" + temp);
+            }catch(JSONException JE){JE.printStackTrace();}
+        }
     }
 }
